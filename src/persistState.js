30e0ab3d84dd8c1a7e7a3dde2b4c7a01817ef561
devbucket <flomueller1981@gmail.com>
2020-10-03 15:57:23 +0200

    Added possibility to pass callback for the store paths.

--- src/persistState.js
+++ src/persistState.js
@@ -28,7 +28,9 @@ function getSubset(state, paths = defaultPaths) {
 /**
  * @param {Object} config The middleware config.
  * @param {string} [config.key='redux'] The storage key.
- * @param {Array} [config.paths=[]] The paths to store. It persists the whole state of not set.
+ * @param {Array|Function} [config.paths=[]] The paths to store. It persists the whole state of
+ * not set. It can be a callback that takes the state as an argument, to check certain conditions
+ * before building the array.
  * @param {Object} [config.adapter=localStorageAdapter] A adapter to user a the storage.
  * @param {Object} [config.logEngine=logger] The log API to use. Defaults to the console API.
  * @return {Function}
@@ -47,7 +49,8 @@ export function persistState(config) {
 
       store.subscribe(() => {
         const state = store.getState();
-        const subset = getSubset(state, paths);
+        const storePaths = (typeof paths === 'function') ? paths(state) : paths;
+        const subset = getSubset(state, storePaths);
 
         adapter.set(key, subset, debounce((error) => {
           if (error) {
